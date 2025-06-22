00c70abb903eca32448504959ab78d7835d6b740
Anna Pastykhova <rainbow261@gmail.com>
2025-06-22 20:39:57 +0300

    hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const API_KEY = '50866532-7da9a026719ecf3bca8b50bdf';
 const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 15;
 
-export async function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page) {
   const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
@@ -11,6 +12,8 @@ export async function getImagesByQuery(query) {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page,
+      per_page: PER_PAGE,
     },
   });
   return response.data;
